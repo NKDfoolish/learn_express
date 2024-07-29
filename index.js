32a2cb335ad33012a23c7fb8414dda7434cfe91d
NKDfoolish <nguyenkhanhduy.dilinh@gmail.com>
2024-07-29 22:54:48 +0700

    29.07.24.v2

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hi IT_er! Long time no see!')
 })
+
+app.post('/posts', (req, res) => {
+    res.send('Return posts successfully')
+  })
+
+app.get('/post/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`This is post with id: ${id}`)
+  })
+
+app.post('/post/:id', (req, res) => {
+    const result = {
+        data: req.body,
+        message: 'Post created successfully'
+    }
+    res.send(result);
+  })
+
+app.put('/post/:id', (req, res) => {
+    const result = {
+        data: req.body,
+        message: 'Put created successfully'
+    }
+    res.send(result);
+  })
+
+app.patch('/post/:id', (req, res) => {
+    const result = {
+        data: req.body,
+        message: 'Patch created successfully'
+    }
+    res.send(result);
+  })
+
+app.delete('/post/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`Deleted post with id: ${id} successfully!`);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
